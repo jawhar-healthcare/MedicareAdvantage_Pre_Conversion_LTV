9764adc67d8650b6876ea552128d7edcf4343975
rutvik-b <rutvik.bhende@healthcare.com>
2022-05-19 17:48:10 -0400

    Minor updates

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ from sqlalchemy import text
 import psycopg2
 from typing import Optional, Tuple, Union
 
-from utils.load_MA_data import get_MA_data
-from utils.load_journaya_data import get_jornaya_data
-from utils.load_zip_enrch_data import get_zip_enrch_data
-from utils.preprocess_transunion import preprocess_transunion_data
+from utils.utils import (
+    get_MA_data,
+    preprocess_post_conversion_ltv_data,
+    get_jornaya_data, 
+    get_zip_enrch_data, 
+    preprocess_transunion_data
+)
+from utils.preprocess_ma_data import preprocess_ma_data
 
 from warnings import filterwarnings
 filterwarnings("ignore")
@@ -45,182 +49,162 @@ def main():
     ### Medicare Advantage Data ###
 
     ## Load Medicare Advantage Data
-    ma_data = get_MA_data(engine= isc_engine)
+    # ma_data = get_MA_data(engine= isc_engine)
 
-    ## Drop Duplicates, Keep relevant data
-    # ma_data = fill_and_drop_duplicates(df= ma_data, identifiers= ["application_id"])
+    # ## Drop Duplicates, Keep relevant data
+    # # ma_data = fill_and_drop_duplicates(df= ma_data, identifiers= ["application_id"])
 
-    ########
+    # ########
 
-    ### Post-Conversion LTV Data ###
+    # ### Post-Conversion LTV Data ###
 
-    ## Load MA Post-Conversion LTV Model Predictions data
-    post_conv_data = pd.read_csv("data/MA LTV model predictionsFINAL.csv", low_memory=False)
+    # post_conv_data = preprocess_post_conversion_ltv_data(
+    #     data_path= "data/MA LTV model predictionsFINAL.csv"
+    # )
 
-    ## Keep only relevant features from MA Post Conversion LTV Data
-    relevant_features = [
-        'application_id', 
-        'policy_id', 
-        'medicare_number',
-        'cancellation_model_prediction',
-        'probability_of_cancellation',
-        'duration_model_prediction',
-        'LTV', 
-        'coverage_duration'
-    ]
-    irrelevant_features = [feat for feat in post_conv_data.columns if feat not in relevant_features]
+    # ## Merge MA data with Post-Conv LTV data
+    # ma_postconv = pd.merge(
+    #     ma_data, 
+    #     post_conv_data, 
+    #     left_on= ['application_id', 'policy_id'],
+    #     right_on= ['post_raw_application_id', 'post_raw_policy_id'],
+    #     how= 'left',
+    #     suffixes= ("", "_xp")
+    # )
+    # ## Drop Duplicated Column names with "_x" suffix
+    # duplicated_columns = [x for x in ma_postconv.columns if "_xp" in x]
+    # ma_postconv = ma_postconv.drop(columns=duplicated_columns)
 
-    ## Keep only relevant columns
-    post_conv_data = post_conv_data.drop(columns=irrelevant_features)
-
-    ## Convert LTV value Strings to Floats
-    post_conv_data["LTV"] = post_conv_data["LTV"].str.replace('$', '').str.replace(',', '').astype(float)
-
-    ## Add a prefix "post_raw_" to all columns
-    post_conv_data = post_conv_data.add_prefix("post_raw_")
-
-    post_conv_data = post_conv_data.drop_duplicates(keep='last', ignore_index=True)
-
-    ## Merge MA data with Post-Conv LTV data
-    ma_postconv = pd.merge(
-        ma_data, 
-        post_conv_data, 
-        left_on= ['application_id', 'policy_id'],
-        right_on= ['post_raw_application_id', 'post_raw_policy_id'],
-        how= 'left',
-        suffixes= ("", "_xp")
-    )
-    ## Drop Duplicated Column names with "_x" suffix
-    duplicated_columns = [x for x in ma_postconv.columns if "_xp" in x]
-    ma_postconv = ma_postconv.drop(columns=duplicated_columns)
-
-    ## List all unique Lead IDs from MA_POSTCONV dataset
-    lead_ids = tuple(
-        set(
-            ma_postconv[ma_postconv["lead_id"] \
-            .notna()]["lead_id"] \
-            .astype('Int64')
-        )
-    )
+    # ## List all unique Lead IDs from MA_POSTCONV dataset
+    # lead_ids = tuple(
+    #     set(
+    #         ma_postconv[ma_postconv["lead_id"] \
+    #         .notna()]["lead_id"] \
+    #         .astype('Int64')
+    #     )
+    # )
     
-    ########
+    # ########
 
-    ### Jornaya data ###
+    # ### Jornaya data ###
 
-    ## Get Jornaya data
-    jrn_data = get_jornaya_data(leads= lead_ids, engine= hc_engine)
+    # ## Get Jornaya data
+    # jrn_data = get_jornaya_data(leads= lead_ids, engine= hc_engine)
 
-    # jrn_data = fill_and_drop_duplicates(
-    #     df= jrn_data, 
-    #     identifiers=["jrn_boberdoo_lead_id"]
+    # # jrn_data = fill_and_drop_duplicates(
+    # #     df= jrn_data, 
+    # #     identifiers=["jrn_boberdoo_lead_id"]
+    # # )
+
+    # ## Merge Jornaya data with MA_POSTCONV data
+    # ma_postconv_jorn = pd.merge(
+    #     ma_postconv, 
+    #     jrn_data, 
+    #     left_on= ['lead_id'],
+    #     right_on= ['jrn_boberdoo_lead_id'],
+    #     how= 'left',
+    #     suffixes= ("", "_xj")
     # )
 
-    
+    # ## Drop Duplicated Column names with "_j" suffix
+    # duplicated_columns = [x for x in ma_postconv_jorn.columns if "_xj" in x]
+    # duplicated_columns.append('jrn_boberdoo_lead_id')
+    # ma_postconv_jorn = ma_postconv_jorn.drop(columns=duplicated_columns)
 
-    ## Merge Jornaya data with MA_POSTCONV data
-    ma_postconv_jorn = pd.merge(
-        ma_postconv, 
-        jrn_data, 
-        left_on= ['lead_id'],
-        right_on= ['jrn_boberdoo_lead_id'],
-        how= 'left',
-        suffixes= ("", "_xj")
-    )
+    # ########
 
-    ## Drop Duplicated Column names with "_j" suffix
-    duplicated_columns = [x for x in ma_postconv_jorn.columns if "_xj" in x]
-    duplicated_columns.append('jrn_boberdoo_lead_id')
-    ma_postconv_jorn = ma_postconv_jorn.drop(columns=duplicated_columns)
+    # ### Zipcode Data ###
 
-    ########
+    # ## Get Zipcode Enriched Data
+    # zip_data = get_zip_enrch_data(engine= hc_engine)
 
-    ### Zipcode Data ###
+    # # zip_data = fill_and_drop_duplicates(
+    # #     df= zip_data, 
+    # #     identifiers=['zip_zipcode']
+    # # )
 
-    ## Get Zipcode Enriched Data
-    zip_data = get_zip_enrch_data(engine= hc_engine)
+    # zip_data = zip_data.groupby(by= "zip_zipcode").first().reset_index()
 
-    # zip_data = fill_and_drop_duplicates(
-    #     df= zip_data, 
-    #     identifiers=['zip_zipcode']
+    # ## Merge Zipcode data with MA_POSTCONV_JORNAYA data
+    # ma_postconv_jorn_zip = pd.merge(
+    #     ma_postconv_jorn, 
+    #     zip_data, 
+    #     left_on= ['app_zip_code'],
+    #     right_on= ['zip_zipcode'],
+    #     how= 'left',
+    #     suffixes= ("", "_xz")
     # )
 
-    zip_data = zip_data.groupby(by= "zip_zipcode").first().reset_index()
+    # ## Drop Duplicated Column names with "_z" suffix
+    # duplicated_columns = [x for x in ma_postconv_jorn_zip.columns if "_xz" in x]
+    # duplicated_columns.append('zip_zipcode')
+    # ma_postconv_jorn_zip = ma_postconv_jorn_zip.drop(columns=duplicated_columns)
 
-    ## Merge Zipcode data with MA_POSTCONV_JORNAYA data
-    ma_postconv_jorn_zip = pd.merge(
-        ma_postconv_jorn, 
-        zip_data, 
-        left_on= ['app_zip_code'],
-        right_on= ['zip_zipcode'],
-        how= 'left',
-        suffixes= ("", "_xz")
-    )
+    # ## Some preprocessing on merged dataset
+    # ma_postconv_jorn_zip['first_name'] = ma_postconv_jorn_zip["first_name"].str.lower()
+    # ma_postconv_jorn_zip['last_name'] = ma_postconv_jorn_zip["last_name"].str.lower()
 
-    ## Drop Duplicated Column names with "_z" suffix
-    duplicated_columns = [x for x in ma_postconv_jorn_zip.columns if "_xz" in x]
-    duplicated_columns.append('zip_zipcode')
-    ma_postconv_jorn_zip = ma_postconv_jorn_zip.drop(columns=duplicated_columns)
+    # ma_phone_nums = list(ma_postconv_jorn_zip["owner_phone"].unique())
+    # ma_first_names = list(ma_postconv_jorn_zip['first_name'].unique())
+    # ma_last_names = list(ma_postconv_jorn_zip['last_name'].unique())
 
-    ## Some preprocessing on merged dataset
-    ma_postconv_jorn_zip['first_name'] = ma_postconv_jorn_zip["first_name"].str.lower()
-    ma_postconv_jorn_zip['last_name'] = ma_postconv_jorn_zip["last_name"].str.lower()
+    # ########
 
-    ma_phone_nums = list(ma_postconv_jorn_zip["owner_phone"].unique())
-    ma_first_names = list(ma_postconv_jorn_zip['first_name'].unique())
-    ma_last_names = list(ma_postconv_jorn_zip['last_name'].unique())
+    # ### Transunion Data ###
 
-    ########
-
-    ### Transunion Data ###
-
-    ## Get Transunion Data
-    tu_data = preprocess_transunion_data(
-        username= "rutvik_bhende", 
-        password= "0723@RutuJuly",
-        account = "uza72979.us-east-1",
-        phone_numbers_list= ma_phone_nums,
-        first_names_list= ma_first_names,
-        last_names_list= ma_last_names 
-    )
-
-    # tu_data_2 = fill_and_drop_duplicates(
-    #     df= tu_data,
-    #     identifiers= ['tu_PHONE_NUMBER']
+    # ## Get Transunion Data
+    # tu_data = preprocess_transunion_data(
+    #     username= "rutvik_bhende", 
+    #     password= "0723@RutuJuly",
+    #     account = "uza72979.us-east-1",
+    #     phone_numbers_list= ma_phone_nums,
+    #     first_names_list= ma_first_names,
+    #     last_names_list= ma_last_names 
     # )
 
-    tu_data = tu_data.groupby(by= ['tu_PHONE_NUMBER', "tu_FIRST_NAME", "tu_LAST_NAME"]).first().reset_index()
+    # # tu_data_2 = fill_and_drop_duplicates(
+    # #     df= tu_data,
+    # #     identifiers= ['tu_PHONE_NUMBER']
+    # # )
 
-    ## Merge Trandunion data with MA_POSTCONV_JORNAYA_ZIP data
-    ma_postconv_jorn_zip_tu = pd.merge(
-        ma_postconv_jorn_zip, 
-        tu_data, 
-        left_on= ['owner_phone', "first_name", "last_name"],
-        right_on= ['tu_PHONE_NUMBER', "tu_FIRST_NAME", "tu_LAST_NAME"],
-        how= 'left',
-        suffixes= ("", "_xtu")
-    )
+    # tu_data = tu_data.groupby(by= ['tu_PHONE_NUMBER', "tu_FIRST_NAME", "tu_LAST_NAME"]).first().reset_index()
 
-    ## Drop Duplicated Column names with "_tu" suffix
-    duplicated_columns = [x for x in ma_postconv_jorn_zip_tu.columns if "_xtu" in x]
-    duplicated_columns.append('tu_PHONE_NUMBER')
+    # ## Merge Trandunion data with MA_POSTCONV_JORNAYA_ZIP data
+    # ma_postconv_jorn_zip_tu = pd.merge(
+    #     ma_postconv_jorn_zip, 
+    #     tu_data, 
+    #     left_on= ['owner_phone', "first_name", "last_name"],
+    #     right_on= ['tu_PHONE_NUMBER', "tu_FIRST_NAME", "tu_LAST_NAME"],
+    #     how= 'left',
+    #     suffixes= ("", "_xtu")
+    # )
+
+    # ## Drop Duplicated Column names with "_tu" suffix
+    # duplicated_columns = [x for x in ma_postconv_jorn_zip_tu.columns if "_xtu" in x]
+    # duplicated_columns.append('tu_PHONE_NUMBER')
     # duplicated_columns.append("tu_FIRST_NAME")
     # duplicated_columns.append("tu_LAST_NAME")
-    ma_postconv_jorn_zip_tu = ma_postconv_jorn_zip_tu.drop(columns=duplicated_columns)
+    # ma_postconv_jorn_zip_tu = ma_postconv_jorn_zip_tu.drop(columns=duplicated_columns)
 
-    ma_postconv_jorn_zip_tu = ma_postconv_jorn_zip_tu.drop_duplicates(keep='last', ignore_index=True)
+    # ma_postconv_jorn_zip_tu = ma_postconv_jorn_zip_tu.drop_duplicates(keep='last', ignore_index=True)
 
-    ########
-
-
-    ## Modify LTV values. Replace Blanks with Zero.
-    ma_postconv_jorn_zip_tu['mod_LTV'] = (ma_postconv_jorn_zip_tu['post_raw_LTV'] / 1.95).fillna(0)
+    # ########
 
 
+    # ## Modify LTV values. Replace Blanks with Zero.
+    # ma_postconv_jorn_zip_tu['mod_LTV'] = (ma_postconv_jorn_zip_tu['post_raw_LTV'] / 1.95).fillna(0)
 
-    ## Save to file
-    ma_postconv_jorn_zip_tu.to_csv("data/ma_postconv_jorn_zip_tu.csv", index= False)
+    # ## Save to file
+    # ma_postconv_jorn_zip_tu.to_csv("data/ma_postconv_jorn_zip_tu.csv", index= False)
 
-    print(duplicated_columns)
+
+    ## Preprocess MA pre conversion data
+
+    pp = preprocess_ma_data(
+        data_path= "data/ma_postconv_jorn_zip_tu.csv"
+    )
+
+    print(pp.shape)
 
 
 
