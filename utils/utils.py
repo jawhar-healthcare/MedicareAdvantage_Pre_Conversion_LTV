6b4b4933dbab6c277404ccc6203b682fe2c4f380
rutvik-b <rutvik.bhende@healthcare.com>
2022-05-27 10:40:10 -0400

    Updates to code functionality

--- utils/utils.py
+++ utils/utils.py
@@ -42,7 +42,7 @@ def preprocess_post_conversion_ltv_data(data_path: str):
     return post_conv_data
 
 
-def get_MA_data(engine):
+def get_MA_data(engine, save_csv= False):
 
     ma_data_sql = """
     SELECT
@@ -118,13 +118,45 @@ def get_MA_data(engine):
             ).days / 365.2425
         )
     )
+
+    ma_data['age_range'] = ma_data['age'].apply(
+        lambda x: get_age_range(x)
+    )
+
+    ma_data["app_zip_code"] = ma_data["app_zip_code"].where(
+        ma_data['app_zip_code'].str.len() < 6, 
+        ma_data['app_zip_code'].str[:5]
+    )
+
+    ma_data["pol_zip_code"] = ma_data["pol_zip_code"].where(
+        ma_data['pol_zip_code'].str.len() < 6, 
+        ma_data['pol_zip_code'].str[:5]
+    )
+
+    if save_csv:
+        ma_data.to_csv("data/ma_data.csv", index=False)
+
     
     return ma_data
+
+def get_age_range(age: int or float):
+    if age < 65:
+        age_s = "Less than 65"
+    elif age >= 65 and age < 75:
+        age_s = "65 to 75"
+    elif age >= 75 and age < 85:
+        age_s = "75 to 85"
+    elif age >= 85:
+        age_s = "More than 85"
+    else:
+        age_s = "Undefined"
+    return age_s
 
 
 def get_jornaya_data(
     leads: list,
-    engine
+    engine,
+    save_csv= False
 ):
 
     jor_sql = f"""
@@ -146,13 +178,10 @@ def get_jornaya_data(
         WHERE product = 'MEDICARE'
         AND leadid IN {leads}
     ) SELECT bp.lead_id AS boberdoo_lead_id,
-            bp.product AS boberdoo_product,
-            bp.age AS boberdoo_age,
             bp.state,
             bp.amount AS boberdoo_amount,
             bp.source AS boberdoo_source,
             bp.lead_type AS boberdoo_lead_type,
-            bp.tcpa_universal_id AS boberdoo_tcpa_universal_id,
             tje.*
     FROM boberdoo_publisher bp
     LEFT JOIN jornaya_trunc tje
@@ -168,12 +197,8 @@ def get_jornaya_data(
 
     ## Drop irrelevant features
     irrelevant_features = [
-        "jrn_boberdoo_age",
-        "jrn_state",
         "jrn_tracking_file_path",
         "jrn_request_age",
-        "jrn_boberdoo_product",
-        "jrn_boberdoo_tcpa_universal_id",
         "jrn_request_tcpa_universal_id",
         "jrn_request_provider",
         "jrn_request_dob",
@@ -189,23 +214,33 @@ def get_jornaya_data(
     ]
 
     ## Remove any zip feats 
-    irrelevant_features = irrelevant_features + [z for z in jornaya.columns if "zip" in z.lower()]
-    jornaya.drop(columns= irrelevant_features, inplace= True)
+    irrelevant_features = irrelevant_features \
+                            + [z for z in jornaya.columns if "zip" in z.lower()] \
+                                + [z for z in jornaya.columns if z.endswith("rule")]
+
+    jornaya = jornaya.drop(columns= irrelevant_features)
+
+    if save_csv:
+        jornaya.to_csv("data/jornaya.csv", index=False)
 
     return jornaya
 
 
 def get_zip_enrch_data(
-    engine
+    engine, 
+    save_csv= False
 ):
 
-    zip_enriched_sql = """
-    SELECT * FROM data_science.zipcode_data_enriched;
+    zip_sql = """
+    SELECT * FROM data_science.zcta_data;
     """
-    zip_ = pd.read_sql_query(text(zip_enriched_sql), engine)
+    zip_ = pd.read_sql_query(text(zip_sql), engine)
 
     ## Add a prefix "zip_" to all columns
-    zip_ = zip_.add_prefix("zip_")
+    zip_ = zip_.add_prefix("zcta_")
+
+    if save_csv:
+        zip_.to_csv("data/zcta.csv", index=False)
 
     return zip_
 
@@ -249,7 +284,8 @@ def preprocess_transunion_data(
     account: str,
     phone_numbers_list: list,
     first_names_list: list,
-    last_names_list: list
+    last_names_list: list, 
+    save_csv= False
 ):
     """
     """
@@ -281,16 +317,9 @@ def preprocess_transunion_data(
         "tu_EMAIL",
         "tu_DATA_INPUT",
         "tu_DOB",
-        "tu_VERIFY_INPUT",
-        "tu_VERIFY_FIRST_NAME",	
-        "tu_VERIFY_LAST_NAME",	
-        "tu_VERIFY_ADDRESS",
-        "tu_VERIFY_DOB", 
-        "tu_VERIFY_PHONE_NUMBER",
-        "tu_VERIFY_EMAIL",
         "tu_DEMO_AGE_YEARS",
-        "tu_DEMO_AGE_MATCH",
         "tu_ADDRESS",
+        "tu_STATE",
         "tu_STATUS_ID",
         "tu_STATUS_RESULT",
         "tu_MESSAGES",
@@ -300,10 +329,14 @@ def preprocess_transunion_data(
         "tu_TRACKING_DATE",
     ]
 
-    ## Remove any zipcode 
-    irrelevant_features = irrelevant_features + [z for z in trunc_tu_data.columns if "zip" in z.lower()]
+    ## Remove any irrelevant features 
+    irrelevant_features = irrelevant_features \
+        + [t for t in trunc_tu_data.columns if "verify" in t.lower()] \
+            + [t for t in trunc_tu_data.columns if "match" in t.lower()]
 
-    trunc_tu_data.drop(columns= irrelevant_features, inplace= True)
+    trunc_tu_data = trunc_tu_data.drop(columns= irrelevant_features)
 
+    if save_csv:
+        trunc_tu_data.to_csv("data/trunc_tu_data.csv", index=False)
 
     return trunc_tu_data
